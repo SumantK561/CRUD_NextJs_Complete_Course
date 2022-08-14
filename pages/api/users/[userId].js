b44b48a2dadfb3c87c9b7db8bdf357d8a560fc4a
Sumant <sumant.kantamsetty@gmail.com>
2022-08-15 00:50:37 +0530

    Update [userId].js

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
                 break;
             default : 
                 res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
-                res.status(405).end(`Method ${method} Not Allowd`)
+                res.status(405).end(`Method ${method} Not Allowed`)
                 break;
         }
 }
